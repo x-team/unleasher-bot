628b25c611453293088ec92b12b9da9b42453681
jacekelgda <jacekelgda@gmail.com>
2017-12-01 14:51:58 +0100

    Switching goal detail card --closes #38

---
+++ models/interactiveComponent.js
@@ -0,0 +1,37 @@
+const IM_TYPE_DIALOG = 'dialog_submission'
+const IM_TYPE_INTERACTIVE_MSG = 'interactive_message'
+const IM_MSG_TYPE_CURRENT_GOAL = 'select_current_goal'
+const IM_MSG_TYPE_SELECT_OR_CREATE  = 'select_or_create_goal'
+const IM_MSG_TYPE_STATUS_UPDATE = 'unleash_status_update'
+const IM_MSG_TYPE_CREATE_FIRST_GOAL = 'create_first_goal'
+const IM_MSG_TYPE_AFTER_GOAL_CREATED = 'post_goal_created'
+const IM_MSG_TYPE_AFTER_GOAL_SWITCHED = 'post_goal_switched'
+const DEFAULT_GOAL_LVL = 1
+const DEFAULT_GOAL_ICON = 'home'
+const IM_MENU_TYPE = 'select'
+const IM_BUTTON_TYPE = 'button'
+const MKTHX = 'Ok, thx!'
+const ACTION_GOAL_COMPLETED = 1
+const ACTION_MORE_TIME = 0
+const ACTION_SET_GOAL_IN_PROGRESS = 'in_progress'
+const ACTION_CREATE_NEW_GOAL = 'create_new'
+
+export {
+  IM_TYPE_DIALOG,
+  IM_TYPE_INTERACTIVE_MSG,
+  IM_MSG_TYPE_CURRENT_GOAL,
+  IM_MSG_TYPE_SELECT_OR_CREATE ,
+  IM_MSG_TYPE_STATUS_UPDATE,
+  IM_MSG_TYPE_CREATE_FIRST_GOAL,
+  IM_MSG_TYPE_AFTER_GOAL_CREATED,
+  IM_MSG_TYPE_AFTER_GOAL_SWITCHED,
+  DEFAULT_GOAL_LVL,
+  DEFAULT_GOAL_ICON,
+  IM_MENU_TYPE,
+  IM_BUTTON_TYPE,
+  MKTHX,
+  ACTION_GOAL_COMPLETED,
+  ACTION_MORE_TIME,
+  ACTION_SET_GOAL_IN_PROGRESS,
+  ACTION_CREATE_NEW_GOAL
+}
--- util/formatter.js
+++ util/formatter.js
@@ -1,3 +1,5 @@
+import * as interactiveComponent from "../models/interactiveComponent"
+
 const formatGoalDueDate = (date) => {
     return date.substring(0, 10)
 }
@@ -16,7 +18,142 @@ const goalsToOptions = (goals) => {
     return goalOptions
 }
 
+const formatInteractiveComponent = (data) => {
+  let component = []
+  switch (data.callbackId) {
+    case interactiveComponent.IM_MSG_TYPE_AFTER_GOAL_CREATED:
+      component = goalCreatedTemplate(data)
+      break;
+    case interactiveComponent.IM_MSG_TYPE_STATUS_UPDATE:
+      const pretext = `Hi! What is your progress on ${data.name} lvl.${data.level} goal ?`
+      component = goalCardTemplate(data, true, pretext)
+      break;
+    case interactiveComponent.IM_MSG_TYPE_AFTER_GOAL_SWITCHED:
+      component = goalCardTemplate(data, false)
+      break;
+  }
+
+  return component
+}
+
 export {
     formatGoalDueDate,
     goalsToOptions,
+    formatInteractiveComponent
+}
+
+const goalCardTemplate = (data, actions, pretext) => {
+  let template = {
+    pretext,
+    fallback: "Unleash status update",
+    color: "#3AA3E3",
+    attachment_type: "default",
+    callback_id: data.callbackId,
+    fields: [
+      {
+        title: "Name",
+        value: data.name,
+        short: true
+      },
+      {
+        title: "Status",
+        value: data.status,
+        short: true
+      },
+      {
+        title: "Description",
+        value: data.description,
+        short: true
+      },
+      {
+        title: "Level",
+        value: data.level,
+        short: true
+      },
+      {
+        title: "Due date",
+        value: data.dueDate,
+        short: true
+      },
+      {
+        title: "Achieved",
+        value: data.achieved ? "Yes" : "No",
+        short: true
+      }
+    ]
+  }
+
+  if (actions) {
+    template.actions = [
+      {
+        name: "goal_done",
+        text: "Completed",
+        style: "primary",
+        value: 1,
+        type: "button"
+      },
+      {
+        name: "need_more_time",
+        text: "I need more time",
+        value: 0,
+        type: "button"
+      },
+      {
+        name: "goals_list",
+        text: "Switch goal here",
+        type: "select",
+        options: data.dropdownOptions,
+      }
+    ]
+  }
+
+  return [ template ]
+}
+
+const goalCreatedTemplate = (data) => {
+  return [
+      {
+          pretext: "Your goal has been created.",
+          text: "What shal we do next ?",
+          fallback: "Goal created",
+          color: "#3AA3E3",
+          attachment_type: "default",
+          callback_id: data.callbackId,
+          fields: [
+              {
+                  title: "Set as current",
+                  value: "Use this button to set your new goal in-progress"
+              },
+              {
+                  title: "Create another",
+                  value: "Use this button to create another goal"
+              },
+              {
+                  title: "Do nothing",
+                  value: "Use this button to dismiss. I will check up on you later.",
+              }
+          ],
+          actions: [
+              {
+                  name: "in_progress",
+                  text: "Set as current",
+                  style: "primary",
+                  value: data.goalId,
+                  type: "button"
+              },
+              {
+                  name: "create_new",
+                  text: "Create another",
+                  value: 2,
+                  type: "button"
+              },
+              {
+                  name: "dismiss",
+                  text: "Do nothing",
+                  value: 0,
+                  type: "button"
+              }
+          ]
+      }
+  ]
 }
