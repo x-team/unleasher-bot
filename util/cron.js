27f184e03235c7af2b10e7b071eef4f29f3ef088
jacekelgda <jacekelgda@gmail.com>
2018-02-14 10:32:12 +0100

    Change cron to weekly status update

--- util/cron.js
+++ util/cron.js
@@ -1,8 +1,8 @@
 import cron from 'node-cron'
 import { weeklyStatusUpdate } from '../handlers/bot'
 
-// const WEEKLY_STATUS_UPDATE_CRON_INTERVAL = '0 8 * * 1' // every Monday at 8 AM
-const WEEKLY_STATUS_UPDATE_CRON_INTERVAL = '*/2 * * * *'
+const WEEKLY_STATUS_UPDATE_CRON_INTERVAL = '0 8 * * 1' // every Monday at 8 AM
+//const WEEKLY_STATUS_UPDATE_CRON_INTERVAL = '*/2 * * * *'
 
 const startWeeklyStatusUpdateJob = () => {
     cron.schedule(WEEKLY_STATUS_UPDATE_CRON_INTERVAL, async function() {
